d169e1f10bdc4027f9b65e765e963260fc6c3c1a
rakshapriyan <rakshapriyanraksha@gmail.com>
2023-04-09 13:48:05 +0530

    Create-react-a

---
+++ kanyakrafts/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">KanyaKrafts</a>
+      </header>
+      <main>List Products</main>
+    </div>
+  );
+}
+
+export default App;
